566ed5f94cc5e1a3be13c7343dfc8f3fc27b0164
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-06-05 19:12:21 +0530

    Added printPascal.cpp

---
+++ Day 1/printPascal.cpp
@@ -0,0 +1,22 @@
+// Link : https://www.codingninjas.com/codestudio/problems/pascal-s-triangle_8230805?challengeSlug=striver-sde-challenge
+
+vector<vector<long long int>> printPascal(int n)
+{
+    vector<vector<long long int>> ans;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        vector<long long int> v(i + 1, 1);
+
+        // make the changes
+
+        for (int j = 1; j < i; j++)
+        {
+            v[j] = ans[i - 1][j] + ans[i - 1][j - 1];
+        }
+        ans.push_back(v);
+    }
+
+    return ans;
+}
--- Day 1/setZeros.cpp
+++ Day 1/setZeros.cpp
@@ -7,36 +7,41 @@ void setZeros(vector<vector<int>> &matrix)
 
 	// declare vector
 
-	vector<int>x,y;
-
+	vector<int> x, y;
 
 	// Traverse the matrix
 
-	for(int i = 0;i<n;i++){
-		for(int j = 0;j<m;j++){
-			if(matrix[i][j] == 0){
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (matrix[i][j] == 0)
+			{
 				x.push_back(i);
 				y.push_back(j);
 			}
 		}
 	}
 
-
 	// Now make the row => 0
 
-	for(int i = 0; i<x.size();i++){
+	for (int i = 0; i < x.size(); i++)
+	{
 		int index = x[i];
-		for(int j = 0;j<m;j++){
+		for (int j = 0; j < m; j++)
+		{
 			matrix[index][j] = 0;
 		}
 	}
 
 	// Now make the column => 0
 
-	for(int i=0;i<y.size();i++){
+	for (int i = 0; i < y.size(); i++)
+	{
 		int index = y[i];
 
-		for(int j = 0;j<n;j++){
+		for (int j = 0; j < n; j++)
+		{
 			matrix[j][index] = 0;
 		}
 	}
