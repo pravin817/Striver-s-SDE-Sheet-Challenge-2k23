bbf8f6ee105190ebc0f4434673f15d0ebe02cc36
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-06-24 22:57:43 +0530

    Day 11 added

---
+++ Day 11/sortTwoLists.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+/************************************************************
+
+    Following is the linked list node structure.
+
+    template <typename T>
+    class Node {
+        public:
+        T data;
+        Node* next;
+
+        Node(T data) {
+            next = NULL;
+            this->data = data;
+        }
+
+        ~Node() {
+            if (next != NULL) {
+                delete next;
+            }
+        }
+    };
+
+************************************************************/
+
+Node<int> *sortTwoLists(Node<int> *first, Node<int> *second)
+{
+    Node<int> *p1 = first, *p2 = second;
+    Node<int> *dummy = new Node<int>(-1);
+    Node<int> *p3 = dummy;
+    while (p1 != NULL and p2 != NULL)
+    {
+        if (p1->data < p2->data)
+        {
+            p3->next = p1;
+            p1 = p1->next;
+        }
+        else
+        {
+            p3->next = p2;
+            p2 = p2->next;
+        }
+        p3 = p3->next;
+    }
+
+    while (p1 != NULL)
+    {
+        p3->next = p1;
+        p1 = p1->next;
+        p3 = p3->next;
+    }
+
+    while (p2 != NULL)
+    {
+        p3->next = p2;
+        p2 = p2->next;
+        p3 = p3->next;
+    }
+    return dummy->next;
+}
